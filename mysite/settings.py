526c301966c9dede98937066532a5470ff7433e1
braverokmc79 <braverokmc79@gmail.com>
2025-02-04 00:16:43 +0900

    4.django model, model 사용법, api 사용

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls',  # 새로 만든 앱 추가
+    #'polls',  # 새로 만든 앱 추가 중복 등록된 'polls'
 ]
 
 MIDDLEWARE = [
