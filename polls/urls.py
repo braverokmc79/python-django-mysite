74500bd46d7a2e807758a5f57057bd4a772fd51f
braverokmc79 <braverokmc79@gmail.com>
2025-02-04 03:33:19 +0900

    6.django view 사용, html 불러오기 및 연결

--- polls/urls.py
+++ polls/urls.py
@@ -2,7 +2,15 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = [
-    path('', views.index, name='index'),
-   
+app_name="polls"
+
+urlpatterns = [    
+    # ex: http://127.0.0.1:8000/polls/
+    path("", views.index, name="index"),
+    # ex: http://127.0.0.1:8000/polls/1/
+    path("<int:question_id>/", views.detail, name="detail"),
+    # ex: http://127.0.0.1:8000/polls/1/results/
+    path("<int:question_id>/results/", views.results, name="results"),
+    # ex: http://127.0.0.1:8000/polls/1/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote"),   
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,53 @@
-from django.shortcuts import render
-
+from django.shortcuts import render , get_object_or_404
+from .models import Question
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.template import loader
 
+
+# 메인 페이지 (index)
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # 1
+    # return HttpResponse("Hello,world!")
+
+    # 2
+    # last_question_list = Question.objects.order_by('-pub_date')[:5]
+    # output = ', '.join([q.question_text for q in last_question_list])
+    # return HttpResponse(output)
+
+    # 3
+    # latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    # template = loader.get_template("polls/index.html")
+    # context = {
+    #     "latest_question_list": latest_question_list,
+    # }
+    # return HttpResponse(template.render(context, request))
+
+    # 4
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html" , context)
+
+
+# 상세 페이지 (detail)
+def detail(request, question_id):
+    #1
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #   raise Http404("Question does not exist")    
+    # return render(request, "polls/detail.html", {"question" :question} )
+
+    #2    
+    question = get_object_or_404(Question,pk=question_id)    
+    return render(request, "polls/detail.html", {"question": question})
+
+
+# 결과 페이지 (results)
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+# 투표 페이지 (vote)
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
