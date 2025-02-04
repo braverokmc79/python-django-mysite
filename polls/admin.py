141ed9f9210d93b4d50fc3512cd18d6b76a99f85
braverokmc79 <braverokmc79@gmail.com>
2025-02-04 14:39:41 +0900

    10.django customize the admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,36 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+# class QuestionAdmin(admin.ModelAdmin):
+#     fieldsets = [
+#         ("질문 정보", {'fields': ['question_text']}),  
+#         ("날짜 정보", {'fields': ['pub_date']}),          
+#     ]
+
+#admin.site.register(Question, QuestionAdmin)
+#admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("질문 정보",               {'fields': ['question_text']}),  # 질문 내용을 입력하는 필드
+        ("날짜 정보", {'fields': ['pub_date'], 'classes': ['collapse']}),  # 날짜 정보 입력 필드 (접을 수 있음)
+    ]
+    
+    inlines = [ChoiceInline]  # 연결된 Choice 모델을 같은 페이지에서 함께 관리할 수 있도록 설정
+    list_display = ('question_text', 'pub_date', 'was_published_recently')  # 관리자 페이지 목록에서 표시할 필드들 지정
+    list_filter = ['pub_date']  # 필터 옵션을 추가하여 날짜 기준으로 질문을 필터링할 수 있도록 설정
+    search_fields = ['question_text']  # 질문 내용을 검색할 수 있도록 설정
+
+
+admin.site.register(Question, QuestionAdmin)
+
+
+
+
+
+
--- polls/models.py
+++ polls/models.py
@@ -1,27 +1,33 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    question_text = models.CharField(max_length=200, verbose_name="질문 내용")
+    pub_date = models.DateTimeField('게시 날짜')
 
     def __str__(self):
         return self.question_text
     
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="최근 게시 여부", # 관리자 페이지에서 한글 라벨 적용
+    )
     # def was_published_recently(self):
     #    return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
-
+    
 
 
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name="관련 질문")
+    choice_text = models.CharField(max_length=200, verbose_name="선택지 내용")
+    votes = models.IntegerField(default=0, verbose_name="투표 수")
 
     def __str__(self):
         return self.choice_text
