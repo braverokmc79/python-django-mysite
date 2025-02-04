5e83d49b6ee1d1ad2166a91e092d781dfcdf654d
braverokmc79 <braverokmc79@gmail.com>
2025-02-04 12:18:54 +0900

    7.django form 사용, 데이터 받아오는 법, generic view 사용

--- polls/urls.py
+++ polls/urls.py
@@ -6,11 +6,20 @@ app_name="polls"
 
 urlpatterns = [    
     # ex: http://127.0.0.1:8000/polls/
-    path("", views.index, name="index"),
+    #path("", views.index, name="index"),
+    
     # ex: http://127.0.0.1:8000/polls/1/
-    path("<int:question_id>/", views.detail, name="detail"),
+    #path("<int:question_id>/", views.detail, name="detail"),
+    
     # ex: http://127.0.0.1:8000/polls/1/results/
-    path("<int:question_id>/results/", views.results, name="results"),
+    #path("<int:question_id>/results/", views.results, name="results"),
+    
     # ex: http://127.0.0.1:8000/polls/1/vote/
-    path("<int:question_id>/vote/", views.vote, name="vote"),   
+    #path("<int:question_id>/vote/", views.vote, name="vote"),   
+
+
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render , get_object_or_404
-from .models import Question
+from .models import Question , Choice
 # Create your views here.
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404 , HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
+from django.views import generic
 
 
 # 메인 페이지 (index)
@@ -29,6 +31,17 @@ def index(request):
     return render(request, "polls/index.html" , context)
 
 
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """마지막으로 게시된 5개의 질문을 반환합니다."""
+        return Question.objects.order_by("-pub_date")[:5]
+
+
+
+
 # 상세 페이지 (detail)
 def detail(request, question_id):
     #1
@@ -43,11 +56,50 @@ def detail(request, question_id):
     return render(request, "polls/detail.html", {"question": question})
 
 
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+
 # 결과 페이지 (results)
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
 
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+
+
 # 투표 페이지 (vote)
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    #return HttpResponse("You're voting on question %s." % question_id)
+
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):    
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # 게시물 데이터를 성공적으로 처리 한 후에는 항상 HttpResponseRedirect 로  results 페이지로 이동하므로서
+        # 사용자가 뒤로 버튼및 새로고침시 데이터가 두 번 게시되는 것을 방지합니다.
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+
+
+
+# 설문조사 결과 페이지
+def results(reqest, question_id):
+    question =get_object_or_404(Question, pk=question_id)
+    return render(reqest, "polls/results.html", {"question": question})
